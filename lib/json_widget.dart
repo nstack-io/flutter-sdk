fa54060c6ba43c34d6b64912d56f0a14b4356e03
maru <maru@nodesagency.com>
2020-04-25 10:59:05 +0200

    Add experimental code generator
    See https://github.com/flutter/flutter/wiki/Code-generation-in-Flutter
    
    - Generates a .dart widget class based on a .json file

---
+++ lib/json_widget.dart
@@ -0,0 +1,5 @@
+import 'package:build/build.dart';
+import 'package:nstack/src/builder.dart';
+
+/// Creates a [JsonWidgetBuilder]
+Builder jsonWidgetBuilder(BuilderOptions builderOptions) => const JsonWidgetBuilder();
