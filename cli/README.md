053e6a01d123a6916c9a6ba2b62cf310de974514
maru <maru@nodesagency.com>
2020-09-07 20:21:36 +0200

    Bring back our custom CLI

---
+++ cli/README.md
@@ -0,0 +1,12 @@
+NStack Flutter's command-line application.
+
+For now:
+```
+dart nstack.dart --help
+```
+
+Soon:
+```
+pub global activate --source git ...
+nstack --help
+```
---
+++ cli/bin/nstack.dart
@@ -0,0 +1,86 @@
+import 'dart:io';
+
+import 'package:yaml/yaml.dart';
+
+import '../lib/command.dart';
+import '../lib/doctor_interactor.dart';
+import '../lib/option.dart';
+
+void main(List<String> arguments) {
+  if (arguments == null || arguments.isEmpty) {
+    printHelp();
+    return;
+  }
+  // Look for a command
+  switch (arguments?.first) {
+    case HelpCommand.name:
+      return processHelpCommand(arguments);
+    case DoctorCommand.name:
+      return processDoctorCommand(arguments);
+  }
+  // Look for a global option
+  if (Option('help').isPresent(arguments)) {
+    return printHelp();
+  }
+  if (Option('version').isPresent(arguments)) {
+    return printVersion();
+  }
+  // No valid command or option found
+  printUnknownArguments(arguments);
+}
+
+void processDoctorCommand(List<String> arguments) async {
+  final verbose = Option('verbose').isPresent(arguments);
+  await DoctorInteractor().execute(
+    command: DoctorCommand(verbose: verbose),
+  );
+}
+
+void processHelpCommand(List<String> arguments) {
+  // TODO
+  print("Not implemented.");
+}
+
+void printUnknownArguments(List<String> arguments) {
+  if (Option('').isPresent(arguments)) {
+    printUnknownOption(arguments.first);
+  } else {
+    printUnknownCommand(arguments.first);
+  }
+}
+
+void printUnknownOption(String option) => print("""
+Could not find an option with name "$option".
+
+Run 'nstack -h' (or 'nstack <command> -h') for available nstack commands and options.
+""");
+
+void printUnknownCommand(String command) => print("""
+Could not find a command with name "$command".
+
+Run 'nstack -h' (or 'nstack <command> -h') for available nstack commands and options.
+""");
+
+void printHelp() => print("""
+Backend as a Service for Mobile & Web apps.
+www.nstack.io
+    
+Global options:
+-h, --help          Print this usage information.
+    
+Available commands:
+  doctor            Show information about your NStack setup.
+      
+Run "nstack help <command>" for more information about a command.
+Run "nstack help -v" for verbose help output.
+""");
+
+void printVersion() async {
+  final version = await File("../pubspec.yaml").readAsString().then((text) {
+    Map yaml = loadYaml(text);
+    return yaml['version'];
+  }).catchError((error) {
+    return null;
+  });
+  print('NStack CLI $version');
+}
