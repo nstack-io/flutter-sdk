f6de2adf38bec8159412bdbd65140707930d696d
maru <maru@nodesagency.com>
2020-09-08 21:28:16 +0200

    Fetch localize resources and write them into /assets

--- cli/lib/src/build/build_interactor.dart
+++ cli/lib/src/build/build_interactor.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:nstack/src/constants.dart';
+
 import '../config.dart';
 import '../interactor.dart';
 import 'build_command.dart';
@@ -5,13 +11,32 @@ import 'build_command.dart';
 class BuildInteractor implements FutureInteractor<void> {
   @override
   Future<void> execute({BuildCommand command}) async {
-    final config = getConfig();
+    final config = await getConfig();
+    final headers = {
+      'Accept-Language': 'en-US',
+      'X-Application-Id': 'h6wJremI2TGFM88gbLkdyljWQuwf2hxhxvCH',
+      'X-Rest-Api-Key': 'zp2S18H32b67eYAbRQh94tVw76ZzaKKXlHjd',
+      'N-Meta': 'android;local;1.0;1.0;nstackbuilder'
+    };
 
-    // TODO: Fetch Localization
-    // Dependency on NStack :data library
+    // Fetch localize index resources
+    final localizeIndexResources = await http
+        .get(localizeIndexResourcesUrl, headers: headers)
+        .then((response) => jsonDecode(response.body)['data']);
 
-    // TODO: Generate fallback localization files under
-    // Path: assets/nstack/de_DE.json
+    // Fetch localize show resources and write them into /assets
+    localizeIndexResources.forEach((indexResource) async {
+      final url = indexResource['url'];
+      await http.get(url, headers: headers).then((value) async {
+        final resource = jsonDecode(value.body);
+        final data = jsonEncode(resource['data']);
+        final locale = resource['meta']['language']['locale'].toString();
+        final fileName = '$locale.json';
+        final filePath = '$nStackAssetsPath';
+        await Directory(filePath).create(recursive: true);
+        await File('$filePath/$fileName').writeAsString(data);
+      });
+    });
 
     // TODO: Generate NStack instance
     // Path: lib/nstack/nstack.dart (next to nstack.json)
--- cli/lib/src/constants.dart
+++ cli/lib/src/constants.dart
@@ -1,2 +1,7 @@
 const nStackConfigPath = 'lib/nstack';
 const nStackConfigFilePath = '$nStackConfigPath/nstack.json';
+const nStackAssetsPath = 'assets/nstack';
+
+const baseUrl = 'https://nstack.io/api/v2';
+const localizeIndexResourcesUrl =
+    '$baseUrl/content/localize/resources/platforms/mobile?dev=true';
